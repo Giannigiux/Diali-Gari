4a3ab083ab7f9332c7ca2da5cef5f6a1f8451e4e
Gianni Giudice <giannigiux.giudice@gmail.com>
2017-12-09 02:46:43 +0100

    Envoi de mail fonctionnel

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     <body>
       <div id="page">
         <?php
-        $destinataire = 'giannigiudice00@gmail.com';
+        $destinataire = 'dialigari.asso@gmail.com';
 
         if (isset($_POST['envoyer']) && isset($_POST['courriel']) && isset($_POST['objet']) && isset($_POST['message']))
         {
@@ -14,13 +14,26 @@
           {
             if (filter_var($courriel, FILTER_VALIDATE_EMAIL))
             {
-              $from = 'From: <' . $courriel . '> \nMime-Version:\n';
-        	    $from .= ' 1.0\nContent-Type: text/html; charset=UTF-8\n';
+              if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $courriel))
+              {
+                $passage_ligne = "\r\n";
+              }
+              else
+              {
+                $passage_ligne = "\n";
+              }
+              $headers = "From: " . $courriel . $passage_ligne;
+              $headers .= "Reply-to: " . $courriel . $passage_ligne;
+              $headers .= "MIME-Version: 1.0" . $passage_ligne;
+              $headers .= 'Content-type: text/html;' . "\r\n";
+              //$headers .= "\r\nX-Mailer: PHP/".phpversion();
+
               $message = nl2br($message);
-              mail($destinataire, $objet, $message, $from);
+              mail($destinataire, $objet, $message, $headers);
               echo '
                 <span class="success">Votre message a bien été envoyé, merci de votre intérêt.</span><br />
                 Vous allez être redirigé vers l\'accueil..
+
                 <meta http-equiv="refresh" content="5; URL=index.php">
               ';
             }
